ec753f6fd4d117a92c49b4f953e6e302313b4d6f
Swaroop Oggu <swaroop.oggu@gmail.com>
2016-02-15 19:02:03 -0500

    Adding for Machine Learning Project
    P4 - Enron Data set

---
+++ Project4/final_project/helper.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 18:11:35 2016
+
+@author: Swaroop
+"""
+
+import csv
+import matplotlib.pyplot as plt
+import pickle
+import sys
+from numpy import mean
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+sys.path.append("../tools/")
+
+from feature_format import featureFormat
+from feature_format import targetFeatureSplit
+
+from sklearn.feature_selection import SelectKBest
+
+def remove_keys(dict_object, keys):
+    """ removes a list of keys from a dict object """
+    for key in keys:
+        dict_object.pop(key, 0)
+
+def get_k_best(data_dict, features_list, k):
+    """ runs scikit-learn's SelectKBest feature selection
+        returns dict where keys=features, values=scores
+    """
+    data = featureFormat(data_dict, features_list)
+    labels, features = targetFeatureSplit(data)
+
+    k_best = SelectKBest(k=k)
+    k_best.fit(features, labels)
+    scores = k_best.scores_
+    unsorted_pairs = zip(features_list[1:], scores)
+    sorted_pairs = list(reversed(sorted(unsorted_pairs, key=lambda x: x[1])))
+    k_best_features = dict(sorted_pairs[:k])
+    print "{0} best features: {1}\n".format(k, k_best_features.keys())
+    return k_best_features
+    
+def add_networth(data_dict, features_list):
+    """ mutates data dict to add aggregate values from stocks and salary """
+    fields = ['total_stock_value', 'exercised_stock_options', 'salary']
+    for record in data_dict:
+        emp = data_dict[record]
+        is_valid = True
+        for field in fields:
+            if emp[field] == 'NaN':
+                is_valid = False
+        if is_valid:
+            emp['networth'] = sum([emp[field] for field in fields])
+        else:
+            emp['networth'] = 'NaN'
+    features_list += ['networth']
+
+def add_email_features(data_dict, features_list):
+    """ mutates data dict to add aggregate values from stocks and salary """
+    fraction_from_poi_email=dict_to_list(data_dict,"from_poi_to_this_person","to_messages")
+    fraction_to_poi_email=dict_to_list(data_dict,"from_this_person_to_poi","from_messages")
+    count=0
+    for i in data_dict:
+        data_dict[i]["fraction_from_poi_email"]=fraction_from_poi_email[count]
+        data_dict[i]["fraction_to_poi_email"]=fraction_to_poi_email[count]
+        count +=1
+    features_list += ['fraction_from_poi_email'] 
+    features_list += ['fraction_to_poi_email'] 
+def visualize(data_dict, feature_x, feature_y):
+    """ generates a plot of feature y vs feature x, colors poi """
+
+    data = featureFormat(data_dict, [feature_x, feature_y, 'poi'])
+
+    for point in data:
+        x = point[0]
+        y = point[1]
+        poi = point[2]
+        color = 'red' if poi else 'blue'
+        plt.scatter(x, y, color=color)
+    plt.xlabel(feature_x)
+    plt.ylabel(feature_y)
+    plt.show()
+    
+def dict_to_list(data_dictionary,key,normalizer):
+    new_list=[]
+
+    for i in data_dictionary:
+        if data_dictionary[i][key]=="NaN" or data_dictionary[i][normalizer]=="NaN":
+            new_list.append(0.)
+        elif data_dictionary[i][key]>=0:
+            new_list.append(float(data_dictionary[i][key])/float(data_dictionary[i][normalizer]))
+    return new_list
+
+def evaluate_clf(clf, features, labels, num_iters=1000, test_size=0.3):
+    print clf
+    accuracy = []
+    precision = []
+    recall = []
+    first = True
+    for trial in range(num_iters):
+        features_train, features_test, labels_train, labels_test =\
+            cross_validation.train_test_split(features, labels, test_size=test_size)
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        accuracy.append(accuracy_score(labels_test, predictions))
+        precision.append(precision_score(labels_test, predictions))
+        recall.append(recall_score(labels_test, predictions))
+        if trial % 10 == 0:
+            if first:
+                sys.stdout.write('\nProcessing')
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            first = False
+
+    print "done.\n"
+    print "precision: {}".format(mean(precision))
+    print "recall:    {}".format(mean(recall))
+    return mean(precision), mean(recall)
---
+++ Project4/final_project/poi_id.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import sys
+import pickle
+import helper
+from copy import copy
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+op_label = 'poi'
+email_features_list = [
+    # 'email_address', # remit email address; informational label
+    'from_messages',
+    'from_poi_to_this_person',
+    'from_this_person_to_poi',
+    'shared_receipt_with_poi',
+    'to_messages',
+    ]
+financial_features_list = [
+    'bonus',
+    'deferral_payments',
+    'deferred_income',
+    'director_fees',
+    'exercised_stock_options',
+    'expenses',
+    'loan_advances',
+    'long_term_incentive',
+    'other',
+    'restricted_stock',
+    'restricted_stock_deferred',
+    'salary',
+    'total_payments',
+    'total_stock_value',
+]
+# You will need to use more features
+features_list = [op_label] + financial_features_list + email_features_list 
+
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+### Task 2: Remove outliers
+## Total and Travel Agency are evident , For Lochart there are no entries in the dataset
+outlier_keys = ['TOTAL', 'THE TRAVEL AGENCY IN THE PARK', 'LOCKHART EUGENE E']
+helper.remove_keys(data_dict, outlier_keys)
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+my_dataset = copy(data_dict)
+my_feature_list = copy(features_list)
+
+num_features = 10 # 10 for logistic regression, 8 for k-means clustering
+best_features = helper.get_k_best(my_dataset, my_feature_list, num_features)
+my_feature_list = [op_label] + best_features.keys()
+
+###Add new features
+helper.add_networth(my_dataset, my_feature_list)
+helper.add_email_features(my_dataset,my_feature_list)
+print "{0} selected features: {1}\n".format(len(my_feature_list) - 1, my_feature_list[1:])
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, my_feature_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+
+# scale features via min-max
+from sklearn import preprocessing
+scaler = preprocessing.MinMaxScaler()
+features = scaler.fit_transform(features)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.naive_bayes import GaussianNB
+gnb_clf = GaussianNB()
+
+### Logistic Regression Classifier
+from sklearn.linear_model import LogisticRegression
+l_clf = LogisticRegression(C=10**18, tol=10**-21)
+
+### K-means Clustering
+from sklearn.cluster import KMeans
+k_clf = KMeans(n_clusters=2, tol=0.001)
+
+### Adaboost Classifier
+from sklearn.ensemble import AdaBoostClassifier
+a_clf = AdaBoostClassifier(algorithm='SAMME')
+
+### Support Vector Machine Classifier
+from sklearn.svm import SVC
+s_clf = SVC(kernel='rbf', C=1000)
+
+### Random Forest
+from sklearn.ensemble import RandomForestClassifier
+rf_clf = RandomForestClassifier()
+
+### Stochastic Gradient Descent - Logistic Regression
+from sklearn.linear_model import SGDClassifier
+g_clf = SGDClassifier(loss='log')
+
+helper.evaluate_clf(l_clf, features, labels)
+helper.evaluate_clf(k_clf, features, labels)
+helper.evaluate_clf(gnb_clf, features, labels)
+
+### Final Machine Algorithm Selection
+clf = l_clf
+
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
